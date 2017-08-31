22a587f7d428e7307bd43c9217805ce9d97e45e0
chenjinjun <1981367757@qq.com>
2017-08-31 18:06:37 +0800

    [fix] bugly

--- app/src/main/java/com/example/music/App.java
+++ app/src/main/java/com/example/music/App.java
@@ -1,33 +1,137 @@
 package com.example.music;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.support.multidex.MultiDex;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.example.commonlibrary.BaseApplication;
-import com.tencent.tinker.loader.app.ApplicationLike;
-import com.tinkerpatch.sdk.TinkerPatch;
-import com.tinkerpatch.sdk.loader.TinkerPatchApplicationLike;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.BuglyStrategy;
+import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.beta.interfaces.BetaPatchListener;
+import com.tencent.bugly.beta.upgrade.UpgradeStateListener;
+
+import java.util.Locale;
+
 
 /**
  * Created by COOTEK on 2017/8/30.
  */
 
 public class App extends BaseApplication {
-
-
-    private ApplicationLike tinkerApplicationLike;
-
     @Override
     public void onCreate() {
         super.onCreate();
-        // 我们可以从这里获得Tinker加载过程的信息
-        tinkerApplicationLike = TinkerPatchApplicationLike.getTinkerPatchApplicationLike();
 
-        // 初始化TinkerPatch SDK, 更多配置可参照API章节中的,初始化SDK
-        TinkerPatch.init(tinkerApplicationLike)
-                .reflectPatchLibrary()
-                .setPatchRollbackOnScreenOff(true)
-                .setPatchRestartOnSrceenOff(true)
-                .setFetchPatchIntervalByHours(3);
+        setStrictMode();
+        // 设置是否开启热更新能力，默认为true
+        Beta.enableHotfix = true;
+        // 设置是否自动下载补丁
+        Beta.canAutoDownloadPatch = true;
+        // 设置是否提示用户重启
+        Beta.canNotifyUserRestart = true;
+        // 设置是否自动合成补丁
+        Beta.canAutoPatch = true;
 
-        // 每隔3个小时(通过setFetchPatchIntervalByHours设置)去访问后台时候有更新,通过handler实现轮训的效果
-        TinkerPatch.with().fetchPatchUpdateAndPollWithInterval();
+        /**
+         *  全量升级状态回调
+         */
+        Beta.upgradeStateListener = new UpgradeStateListener() {
+            @Override
+            public void onUpgradeFailed(boolean b) {
+
+            }
+
+            @Override
+            public void onUpgradeSuccess(boolean b) {
+
+            }
+
+            @Override
+            public void onUpgradeNoVersion(boolean b) {
+                Toast.makeText(getApplicationContext(), "最新版本", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUpgrading(boolean b) {
+                Toast.makeText(getApplicationContext(), "onUpgrading", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownloadCompleted(boolean b) {
+
+            }
+        };
+
+        /**
+         * 补丁回调接口，可以监听补丁接收、下载、合成的回调
+         */
+        Beta.betaPatchListener = new BetaPatchListener() {
+            @Override
+            public void onPatchReceived(String patchFileUrl) {
+                Toast.makeText(getApplicationContext(), patchFileUrl, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownloadReceived(long savedLength, long totalLength) {
+                Toast.makeText(getApplicationContext(), String.format(Locale.getDefault(),
+                        "%s %d%%",
+                        Beta.strNotificationDownloading,
+                        (int) (totalLength == 0 ? 0 : savedLength * 100 / totalLength)), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownloadSuccess(String patchFilePath) {
+                Toast.makeText(getApplicationContext(), patchFilePath, Toast.LENGTH_SHORT).show();
+//                Beta.applyDownloadedPatch();
+            }
+
+            @Override
+            public void onDownloadFailure(String msg) {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onApplySuccess(String msg) {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onApplyFailure(String msg) {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPatchRollback() {
+                Toast.makeText(getApplicationContext(), "onPatchRollback", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        long start = System.currentTimeMillis();
+        // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId,调试时将第三个参数设置为true
+        Bugly.init(this, "2e5309db50", true);
+        long end = System.currentTimeMillis();
+        Log.e("init time--->", end - start + "ms");
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        // you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+        // 安装tinker
+        Beta.installTinker();
+    }
+
+
+    @TargetApi(9)
+    protected void setStrictMode() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build());
     }
 }
--- app/src/main/java/com/example/music/MainActivity.java
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.music;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.example.commonlibrary.utils.CommonLogger;
+import com.example.commonlibrary.utils.ToastUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void jump(View view) {
+        Toast.makeText(this, "升级", Toast.LENGTH_SHORT).show();
         ARouter.getInstance().build("/video/main").navigation();
 
+    }
+
+
+    public void crash(View view) {
+        ToastUtils.showShortToast("这是基线版本的补丁+陈锦军");
     }
 }
